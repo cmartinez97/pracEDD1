9984489c977a086b335fa3c237524a655e802278
Christian Martinez Grau <chrmargra.smx@ieseljust.com>
2018-04-26 17:15:10 +0200

    Modificación

--- repoEDD1/src/repoedd1/RepoEDD1.java
+++ repoEDD1/src/repoedd1/RepoEDD1.java
@@ -11,17 +11,10 @@ public class RepoEDD1 {
         for (int i = 0; i < 10; i++) {
             System.out.println("Hola Mundo!");
         }
-        System.out.println("LÃ­nea AÃ±adida desde NetBeans");
         System.out.println("Hello World!");
         System.out.println("IDE Eclipse");
-<<<<<<< HEAD
         System.out.println("Nueva línea escrita en Eclipse");
         System.out.println("Más líneas escritas des del Eclipse, las cuales se subirán al GitHub");
-=======
-        System.out.println("Nueva lï¿½nea escrita en Eclipse");
-        System.out.println("Otra linea para que asÃ­ no de error");
-        System.out.println("Hola");
->>>>>>> refs/remotes/origin/master
     }
-        
+
 }
